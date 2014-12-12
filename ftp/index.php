f2898ddf22b032b4af6efd895cf164e20c5f137d
nklnkl <lagman.niko@gmail.com>
2014-12-12 00:44:24 -0500

    added mobile view meta viewport
    Added the meta tags necessary for mobile view compatibility

--- ftp/index.php
+++ ftp/index.php
@@ -22,6 +22,12 @@
 		<!-- Google Fonts -->
 		<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:200' rel='stylesheet' type='text/css'>
 		
+		<!-- Mobile View Compatibility -->
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta name="apple-mobile-web-app-title" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+		
 		<link href="css/style.css" rel="stylesheet" type="text/css" />
 		
 		<script src="js/script.js" type="text/javascript"></script>
