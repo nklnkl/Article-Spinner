fb42798162ecddd1fa709134a38c891070bf2135
nklnkl <lagman.niko@gmail.com>
2015-11-22 02:43:21 -0500

    Added the prototype controller/router function to handle receiving POST requests for spinning text. Sending a POST request with a json, with a key named article and its value being the article to be spun, will send back a json with a spinned article. It's not perfect, will work on it further.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var articles = require('./routes/articles');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api/users', users);
+app.use('/api/articles', articles);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
